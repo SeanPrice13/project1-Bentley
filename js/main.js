8b0c25e9d3de9b330a2a78aac100c046107b4902
SeanPrice13 <bentleyprice@live.com>
2022-02-04 18:28:02 -0400

    replaced queryselector with body.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const colors = ['red', 'black', 'green', 'yellow', 'purple'];
 
-const changeBgColor = () => {document.querySelector('body').style.background = colors[Math.floor(Math.random()*colors.length)]}
+const changeBgColor = () => {
+    document.body.style.background = colors[Math.floor(Math.random() * colors.length)]
+};
 
 document.querySelector('button').addEventListener('click', changeBgColor);
